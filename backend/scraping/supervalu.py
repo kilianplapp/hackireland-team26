6dcd224191c904331163ed6168c1075dadc2ba53
Kilian Plapp <kilianplapp@gmail.com>
2025-02-22 16:15:40 +0000

    dunnes + supervalu image update

--- backend/scraping/supervalu.py
+++ backend/scraping/supervalu.py
@@ -4,10 +4,13 @@ def supervalu(query):
     url = f'https://storefrontgateway.supervalu.ie/api/stores/5550/preview?q={query}&popularTake=30'
     response = requests.get(url)
     data = response.json()
-    products = {}
+    products = []
     for product in data['products']:
-        products[product['name']] = product['price']
-    print(products)
+        products.append({
+            'title': product['name'],
+            'price': product['price'],
+            'image': product['image']['default']
+        })
     return products
 
 if __name__ == '__main__':
