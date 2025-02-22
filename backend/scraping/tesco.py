e714470cbe25bc6d9da1cea6eaa375d85cf6deaa
Kilian Plapp <kilianplapp@gmail.com>
2025-02-22 16:06:07 +0000

    tesco image update

--- backend/scraping/tesco.py
+++ backend/scraping/tesco.py
@@ -1,5 +1,5 @@
 from patchright.sync_api import sync_playwright
-
+import json
 def tesco(query):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
@@ -7,19 +7,26 @@ def tesco(query):
         page.goto(f'https://www.tesco.ie/groceries/en-IE/search?query={query}')
         
         # Get all product title elements and price elements
-        titles = page.query_selector_all("//*/div[2]/h3/a")
-        prices = page.query_selector_all("//*/div/div/div/div/div/form/div/div/div[1]/p[1]")
+        product_boxes = page.query_selector_all('//*[@id="product-list"]/div[2]/div[4]/div/div/div[2]/div/ul/li')
         
-        # Combine titles and prices into a dictionary
-        products = {}
-        for title, price in zip(titles, prices):
-            title_text = title.inner_text().strip()
-            price_text = price.inner_text().strip()
-            products[title_text] = price_text
+        products = []
+        for product_box in product_boxes:
+            title = product_box.query_selector("//*/div[2]/h3/a")
+            price = product_box.query_selector("//*/div/div/div/div/div/form/div/div/div[1]/p[1]")
+            image = product_box.query_selector("//img")
+            
+            title_text = title.inner_text().strip() if title else "No title"
+            price_text = price.inner_text().strip() if price else "€0.00"
+            image_src = image.get_attribute("srcset").split(' ')[0] if image else "No image"
+            
+            products.append({
+                "title": title_text,
+                "price": price_text,
+                "image": image_src
+            })
         #page.screenshot(path=f'example-{p.chromium.name}.png')
         browser.close()
-
         return products
 
 if __name__ == '__main__':
-    tesco("carrots")
+    print(tesco("carrots"))
